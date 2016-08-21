8316e6e819719fa82188fd024c6da42b5a60bb33
syam <syam.s@outlook.com>
2016-08-21 19:42:29 +0200

    updated the capabitites to set marionette to false

--- src/test/java/TestCases/TestCases.java
+++ src/test/java/TestCases/TestCases.java
@@ -1,13 +1,21 @@
 package TestCases;
 
+import HomePage.homePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by Syam on 21-08-2016.
  */
 public class TestCases {
-    public static WebDriver driver;
-    public static void main(String [] args){
 
+
+    public static void main(String [] args){
+        System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "false");
+        WebDriver driver= new FirefoxDriver();
+        driver.navigate().to("https://www.google.com/ncr");
+    homePage homePage = new homePage(driver);
+        homePage.txtSearch.sendKeys("Selenium");
+        homePage.btnSearch.click();
     }
 }
