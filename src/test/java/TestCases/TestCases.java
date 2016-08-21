4d3b2853ebc3fbe3ccfec7f769e048f5a665faae
syam <syam.s@outlook.com>
2016-08-21 18:37:26 +0200

    New folders

---
+++ src/test/java/TestCases/TestCases.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Syam on 21-08-2016.
+ */
+public class TestCases {
+    public static WebDriver driver;
+    public static void main(String [] args){
+
+    }
+}
