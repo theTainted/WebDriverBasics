b250422f40bd44ca1d15585d4be17e836946c765
syam <syam.s@outlook.com>
2016-08-28 13:03:11 +0200

    Added  a new method  in HomePage.Java to click on selenium link (search results) Created a new class file SeleniumPageObjects to add objects that we see when we clik on the selenium link when we search for selenium

--- src/test/java/TestCases/TestCases.java
+++ src/test/java/TestCases/TestCases.java
@@ -1,8 +1,10 @@
 package TestCases;
 
-import HomePage.homePage;
+import PageObjects.HomePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Syam on 21-08-2016.
@@ -14,7 +16,9 @@ public class TestCases {
         System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "false");
         WebDriver driver= new FirefoxDriver();
         driver.navigate().to("https://www.google.com/ncr");
-    homePage homePage = new homePage(driver);
+        HomePage homePage = new HomePage(driver);
        homePage.SearchGoogle("Selenium");
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        homePage.clickSelenium();
     }
 }
