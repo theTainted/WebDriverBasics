3d5a44484237b45ee375f2b4d0cf17ea7cfc6fdf
syam.suryanarayanan <syam.s@outlook.com>
2016-09-12 10:42:29 +0200

    Added COPYContactFormSegmentPaymentServiceProviders and testng xml to test if i can run multiple files.

---
+++ src/test/java/TestCases/COPYContactFormSegmentPaymentServiceProviders.java
@@ -0,0 +1,41 @@
+package TestCases;
+
+import PageObjects.ContactForm;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by syam.suryanarayanan on 9/12/2016.
+ */
+public class COPYContactFormSegmentPaymentServiceProviders {
+    static String sURL = "https://www-ingenico-test-global.lostboys.nl/br/epayments/contato";
+
+    @Test
+    public void testContactForm() {
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to(sURL);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        ContactForm contactform = new ContactForm(driver);
+        contactform.switchToFrame(0);
+        contactform.setFirstName("Syam");
+        contactform.setLastName("s");
+        contactform.setJobTitle("tester");
+        contactform.setCompany("LBi");
+        contactform.setEmail("syam@LBi.com");
+        contactform.setPhone("1234567");
+        contactform.selectIndustry(1);
+        contactform.selectSegementForFinancialIndustry(2);
+        contactform.countrySelector(2);
+        contactform.selectSalesVolume(1);
+        contactform.setMessageInMessageBox("Test Message");
+        contactform.clickSubmit();
+    }
+}
+
