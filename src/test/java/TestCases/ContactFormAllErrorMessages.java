63414efbf1a31b2552792f3a4c7b585200ba52cc
syam <syam.s@outlook.com>
2016-09-12 21:54:30 +0200

    added PT.txt added switch case

--- src/test/java/TestCases/ContactFormAllErrorMessages.java
+++ src/test/java/TestCases/ContactFormAllErrorMessages.java
@@ -5,17 +5,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by syam.suryanarayanan on 9/12/2016.
  */
 public class ContactFormAllErrorMessages {
+    static Properties property = new Properties();
     static String sURL = "https://www-ingenico-test-global.lostboys.nl/br/epayments/contato";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
@@ -25,12 +30,21 @@ public class ContactFormAllErrorMessages {
 
         ContactForm contactform = new ContactForm(driver);
         contactform.switchToFrame(0);
-       contactform.txtBoxFirstName.click();
         contactform.clickSubmit();
         List<WebElement> listOfErrorMessage= driver.findElements(By.xpath("//div[@id='feedbackPanel']//li"));
         System.out.println(listOfErrorMessage.size());
         for(WebElement errorMessage:listOfErrorMessage){
-            System.out.println(errorMessage.getText());
+
+            switch (sURL) {
+                case "https://www-ingenico-test-global.lostboys.nl/br/epayments/contato":
+                    property.load(new FileInputStream("C:\\Users\\Syam\\IdeaProjects\\WebDriverBasics\\PT.txt"));
+
+            }
+
+            Assert.assertEquals(property.getProperty("Empty_First_Name"), listOfErrorMessage.get(0).getText(), "First Name");
+            Assert.assertEquals(property.getProperty("Empty_Last_Name"), errorMessage.getText(), "Last Name");
+
+
         }
     }
 }
