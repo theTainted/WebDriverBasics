d0a086a7e725b65ecc1627fdbc6e344f09937719
syam.suryanarayanan <syam.s@outlook.com>
2016-09-16 14:49:16 +0200

    TestCases.TwoToFiveMillionContactFormMerchantServiceProviderSegmentAggregators           TestCases.TwoToFiveMillionContactFormSegmentMerchantServiceProvidersIndependentSalesOrganizations           TestCases.TwotoFiveMillionContactFormSegmentMerchantServiceProvidersIndependentTelcoResellers           TestCases.TwotoFiveMillionContactFormSegmentMerchantServiceProvidersIndependentValueAddedResellers

---
+++ src/test/java/TestCases/TwoToFiveMillionContactFormMerchantServiceProviderSegmentAggregators.java
@@ -0,0 +1,44 @@
+package TestCases;
+
+import PageObjects.ContactForm;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Syam on 10-09-2016.
+ */
+public class TwoToFiveMillionContactFormMerchantServiceProviderSegmentAggregators {
+    String sURL = ContactForm.sURL;
+
+  //  public static void main(String[] args) {
+    @Test
+       public void testTwoMillionContactFormSegmentAggregators(){
+    System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to(sURL);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        ContactForm contactform = new ContactForm(driver);
+        contactform.switchToFrame(0);
+        contactform.setFirstName("Syam");
+        contactform.setLastName("s");
+        contactform.setJobTitle("tester");
+        contactform.setCompany("LBi");
+        contactform.setEmail("syam@LBi.com");
+        contactform.setPhone("1234567");
+        contactform.selectIndustry(2);
+        contactform.selectSegmentForMerchantsServicesProviders(1);
+        contactform.countrySelector(13);
+        contactform.selectSalesVolume(2);
+        contactform.selectTargetMarket("Asia/Pacific");
+        contactform.selectTargetMarket("Europe");
+        contactform.setMessageInMessageBox("Test Message");
+        contactform.clickSubmit();
+        driver.quit();
+    }
+}
---
+++ src/test/java/TestCases/TwotoFiveMillionContactFormSegmentMerchantServiceProvidersIndependentValueAddedResellers.java
@@ -0,0 +1,44 @@
+package TestCases;
+
+import PageObjects.ContactForm;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Syam on 10-09-2016.
+ */
+public class TwotoFiveMillionContactFormSegmentMerchantServiceProvidersIndependentValueAddedResellers {
+    String sURL = ContactForm.sURL;
+
+  //  public static void main(String[] args) {
+    @Test
+       public void testwotoFiveMillionContactFormSegmentMerchantServiceProvidersIndependentValueAddedResellers(){
+    System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to(sURL);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        ContactForm contactform = new ContactForm(driver);
+        contactform.switchToFrame(0);
+        contactform.setFirstName("Syam");
+        contactform.setLastName("s");
+        contactform.setJobTitle("tester");
+        contactform.setCompany("LBi");
+        contactform.setEmail("syam@LBi.com");
+        contactform.setPhone("1234567");
+        contactform.selectIndustry(2);
+        contactform.selectSegmentForMerchantsServicesProviders(4);
+         contactform.countrySelector(19);
+        contactform.selectSalesVolume(2);
+        contactform.selectTargetMarket("Asia/Pacific");
+        contactform.selectTargetMarket("Middle East");
+        contactform.setMessageInMessageBox("Test Message");
+        contactform.clickSubmit();
+        driver.quit();
+    }
+}
