643e3e3a2b9c39517540eb6157d14dd50ea91f6e
syam.suryanarayanan <syam.s@outlook.com>
2016-09-21 08:58:31 +0200

    TwoMillionContactFormRetailSegmentDepartmentStores

---
+++ src/test/java/TestCases/TwoMillionContactFormRetailSegmentDepartmentStores.java
@@ -0,0 +1,44 @@
+package TestCases;
+
+import PageObjects.ContactForm;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Syam on 10-09-2016.
+ */
+public class TwoMillionContactFormRetailSegmentDepartmentStores {
+ // private static String sURL = "https://www-ingenico-test-global.lostboys.nl/br/epayments/contato";
+        String sURL = ContactForm.sURL;
+  //  public static void main(String[] args) {
+    @Test
+       public void testTwoMillionContactFormRetailSegmentDepartmentStores(){
+
+    System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to(sURL);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        ContactForm contactform = new ContactForm(driver);
+       // driver.navigate().to(sURL);
+        contactform.switchToFrame(0);
+        contactform.setFirstName("Syam");
+        contactform.setLastName("s");
+        contactform.setJobTitle("tester");
+        contactform.setCompany("LBi");
+        contactform.setEmail("syam@LBi.com");
+        contactform.setPhone("1234567");
+        contactform.selectIndustry(3);
+        contactform.selectSegmentForRetail(6);
+        contactform.countrySelector(30);
+        contactform.selectSalesVolume(1);
+        contactform.setMessageInMessageBox("Test Message");
+        contactform.clickSubmit();
+        driver.quit();
+    }
+}
