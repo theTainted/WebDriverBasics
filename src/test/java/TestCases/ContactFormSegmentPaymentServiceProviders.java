7e3ee860098b80ce0362725c00c81b9eb8a3197d
syam.suryanarayanan <syam.s@outlook.com>
2016-09-30 10:55:00 +0200

    testAvailabilityOfCurrencyDropDown

--- src/test/java/TestCases/ContactFormSegmentPaymentServiceProviders.java
+++ src/test/java/TestCases/ContactFormSegmentPaymentServiceProviders.java
@@ -31,7 +31,7 @@ public class ContactFormSegmentPaymentServiceProviders {
         contactform.setPhone("1234567");
         contactform.selectIndustry("Merchants Financial Services Providers");
         contactform.selectSegementForFinancialIndustry("Payment Service Providers");
-        contactform.countrySelector("AX");
+        contactform.countrySelector("Ghana");
         contactform.selectSalesVolume("New business - 2 million");
         contactform.setMessageInMessageBox("Test Message");
         contactform.clickSubmit();
