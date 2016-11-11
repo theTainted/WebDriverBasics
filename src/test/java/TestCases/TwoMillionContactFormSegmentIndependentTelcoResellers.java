c9fe84dd8c814f084a3a4197412026dbafb73be2
syam.suryanarayanan <syam.s@outlook.com>
2016-11-11 09:38:12 +0100

    Created a NL error properties file. and added in the contactformallerrormessages testcase

--- src/test/java/TestCases/TwoMillionContactFormSegmentIndependentTelcoResellers.java
+++ src/test/java/TestCases/TwoMillionContactFormSegmentIndependentTelcoResellers.java
@@ -33,7 +33,7 @@ public class TwoMillionContactFormSegmentIndependentTelcoResellers {
         contactform.setPhone("1234567");
         contactform.selectIndustry("Merchants Services Providers");
         contactform.selectSegmentForMerchantsServicesProviders("Telco resellers");
-        contactform.countrySelector("Baharin");
+        contactform.countrySelector("Bahrain");
         contactform.selectSalesVolume("New business - 2 million");
         contactform.setMessageInMessageBox("Test Message");
         contactform.clickSubmit();
