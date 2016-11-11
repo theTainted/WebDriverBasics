591038ebcb32ece76f99b215efc40cfd6987be61
syam.suryanarayanan <syam.s@outlook.com>
2016-11-11 10:16:20 +0100

    Created a NL error properties file. and added in the contactformallerrormessages testcase

--- src/test/java/PageObjects/ContactForm.java
+++ src/test/java/PageObjects/ContactForm.java
@@ -63,7 +63,7 @@ public class ContactForm {
 
     WebDriver driver;
     SoftAssert softAssert = new SoftAssert();
-    public static String sURL="https://nl-ingenico-test-global.lostboys.nl/epayments/contact";
+    public static String sURL="https://www-ingenico-staging-global.lostboys.nl/lar-es/epayments/contacto";
     public ContactForm(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
